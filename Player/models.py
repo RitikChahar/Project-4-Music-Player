bbc6b0e509b05470cf9dc7605e3da4b0f007a684
RitikChahar <ritickchahar@gmail.com>
2025-05-07 19:19:24 +0530

    Added filter and metadata API

--- Player/models.py
+++ Player/models.py
@@ -2,6 +2,9 @@ import uuid
 from django.db import models
 from django.db.models import JSONField
 
+def _lower_list(values):
+    return [v.lower() if isinstance(v, str) else v for v in values]
+
 class Song(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
@@ -9,6 +12,16 @@ class Song(models.Model):
     artists = JSONField(default=list, blank=True)
     genre = JSONField(default=list, blank=True)
     language = JSONField(default=list, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.title:
+            self.title = self.title.lower()
+        if self.album:
+            self.album = self.album.lower()
+        self.artists = _lower_list(self.artists)
+        self.genre = _lower_list(self.genre)
+        self.language = _lower_list(self.language)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -19,6 +32,13 @@ class Metadata(models.Model):
     artists = JSONField(default=list, blank=True)
     genre = JSONField(default=list, blank=True)
     language = JSONField(default=list, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.album = _lower_list(self.album)
+        self.artists = _lower_list(self.artists)
+        self.genre = _lower_list(self.genre)
+        self.language = _lower_list(self.language)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Metadata {self.id}"
--- Player/serializers.py
+++ Player/serializers.py
@@ -1,12 +1,50 @@
 from rest_framework import serializers
-from .models import Song, Metadata
+from .models import Song, Metadata, _lower_list
 
 class SongSerializer(serializers.ModelSerializer):
+    title   = serializers.CharField()
+    album   = serializers.CharField()
+    artists = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    genre   = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    language= serializers.ListField(child=serializers.CharField(), allow_empty=True)
+
+    def validate_title(self, value):
+        return value.lower()
+
+    def validate_album(self, value):
+        return value.lower()
+
+    def validate_artists(self, value):
+        return _lower_list(value)
+
+    def validate_genre(self, value):
+        return _lower_list(value)
+
+    def validate_language(self, value):
+        return _lower_list(value)
+
     class Meta:
         model = Song
         fields = ['id', 'title', 'album', 'artists', 'genre', 'language']
 
 class MetadataSerializer(serializers.ModelSerializer):
+    album    = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    artists  = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    genre    = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    language = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+
+    def validate_album(self, value):
+        return _lower_list(value)
+
+    def validate_artists(self, value):
+        return _lower_list(value)
+
+    def validate_genre(self, value):
+        return _lower_list(value)
+
+    def validate_language(self, value):
+        return _lower_list(value)
+
     class Meta:
         model = Metadata
         fields = ['id', 'album', 'artists', 'genre', 'language']
